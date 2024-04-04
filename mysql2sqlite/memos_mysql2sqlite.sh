3fdca3d9407e3a239930e6c06cfb3669ec4990c8
tigerzioo <148376507+tigerzioo@users.noreply.github.com>
2024-04-04 15:24:58 -0500

    Under development

--- mysql2sqlite/memos_mysql2sqlite.sh
+++ mysql2sqlite/memos_mysql2sqlite.sh
@@ -6,8 +6,11 @@ mysql -u root --disable-column-names -B -e "select 'INSERT INTO user (id,created
 # remove the comma at the end of the file
 sed -i '$ s/.$//' memos_2sqlite.sql
 
+# TAG
+mysqldump -u root --no-create-info --skip-triggers --compact --extended-insert=FALSE memos_prod tag >> memos_2sqlite.sql
+
 # RESOURCE
-mysql -u root --disable-column-names -B -e "select 'INSERT INTO resource (id, resource_name, creator_id, created_ts, updated_ts, filename, blob, external_link, type, size, internal_path, memo_id) VALUES' union all SELECT concat('(',id,',''',resource_name,''',',creator_id,',',UNIX_TIMESTAMP(created_ts),',',UNIX_TIMESTAMP(updated_ts),',''',filename,''',',concat('x''',hex(\`blob\`)),''',''',external_link,''',''',type,''',',size,',''',internal_path,''',',memo_id,'),') as iq FROM memos_prod.resource" > memos_2sqlite.sql
+mysql -u root --disable-column-names -B -e "select 'INSERT INTO resource (id, resource_name, creator_id, created_ts, updated_ts, filename, blob, external_link, type, size, internal_path, memo_id) VALUES' union all SELECT concat('(',id,',''',resource_name,''',',creator_id,',',UNIX_TIMESTAMP(created_ts),',',UNIX_TIMESTAMP(updated_ts),',''',filename,''',',concat('x''',hex(\`blob\`)),''',''',external_link,''',''',type,''',',size,',''',internal_path,''',',memo_id,'),') as iq FROM memos_prod.resource" >> memos_2sqlite.sql
 
 sed -i '$ s/.$//' memos_2sqlite.sql
 
