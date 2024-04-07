7718219214e7e087a00bd23c3ebf628dc341e8fb
tigerzioo <148376507+tigerzioo@users.noreply.github.com>
2024-04-06 20:52:56 -0500

    Added MySQL database name input

--- sqlite2mysql/memos_sqlite2mysql.sh
+++ sqlite2mysql/memos_sqlite2mysql.sh
@@ -4,7 +4,7 @@ echo " "
 echo " "
 echo "+++++++++++++++++ Memos SQLite 2 MySQL ++++++++++++++++++++++"
 echo "提示："
-echo "SQLite数据库文件必需为memos_prod.db，MySQL数据库名必需为memos_prod。"
+echo "SQLite数据库文件必需为memos_prod.db，MySQL数据库名可自己输入，默认为memos_prod。"
 echo "此Bash脚本文件要和SQLite数据库文件放在同一目录下。"
 echo " "
 columns=12
@@ -31,23 +31,28 @@ fi
 
 done
 
+# read -p "请输入SQLite数据库文件名（不填默认为memos_prod.db）: " litedb
+# if [[ -z "$litedb" ]]; then
+  litedb="memos_prod.db"
+# fi
 
-sqlite3 memos_prod.db ".mode insert user" "select id,datetime(created_ts, 'unixepoch') as created_ts,datetime(updated_ts, 'unixepoch') as updated_ts,row_status,username,role,email,nickname,password_hash,avatar_url,description from user;" > memos_mysql.sql
 
-sqlite3 memos_prod.db ".mode insert tag" "select name,creator_id from tag;" >> memos_mysql.sql
 
-sqlite3 memos_prod.db ".mode insert memo" "SELECT id,uid,creator_id,datetime(created_ts, 'unixepoch') as created_ts,datetime(updated_ts, 'unixepoch') as updated_ts,row_status,content,visibility FROM memo;" >> memos_mysql.sql
+sqlite3 "$litedb" ".mode insert user" "select id,datetime(created_ts, 'unixepoch') as created_ts,datetime(updated_ts, 'unixepoch') as updated_ts,row_status,username,role,email,nickname,password_hash,avatar_url,description from user;" > memos_mysql.sql
 
-sqlite3 memos_prod.db ".mode insert resource" "SELECT id,uid,creator_id,datetime(created_ts, 'unixepoch') as created_ts,datetime(updated_ts, 'unixepoch') as updated_ts,filename,hex(blob),external_link,type,size,internal_path,memo_id FROM resource;" >> memos_mysql.sql
+sqlite3 "$litedb" ".mode insert tag" "select name,creator_id from tag;" >> memos_mysql.sql
 
-sqlite3 memos_prod.db ".mode insert memo_organizer" "select memo_id,user_id,pinned from memo_organizer;" >> memos_mysql.sql
+sqlite3 "$litedb" ".mode insert memo" "SELECT id,uid,creator_id,datetime(created_ts, 'unixepoch') as created_ts,datetime(updated_ts, 'unixepoch') as updated_ts,row_status,content,visibility FROM memo;" >> memos_mysql.sql
 
-sqlite3 memos_prod.db ".mode insert memo_relation" "select memo_id,related_memo_id,type from memo_relation;" >> memos_mysql.sql
+sqlite3 "$litedb" ".mode insert resource" "SELECT id,uid,creator_id,datetime(created_ts, 'unixepoch') as created_ts,datetime(updated_ts, 'unixepoch') as updated_ts,filename,hex(blob),external_link,type,size,internal_path,memo_id FROM resource;" >> memos_mysql.sql
 
-sqlite3 memos_prod.db ".mode insert reaction" "select id,datetime(created_ts, 'unixepoch') as created_ts,creator_id,content_id,reaction_type from reaction;" >> memos_mysql.sql
+sqlite3 "$litedb" ".mode insert memo_organizer" "select memo_id,user_id,pinned from memo_organizer;" >> memos_mysql.sql
 
-sqlite3 memos_prod.db ".mode insert inbox" "SELECT id,datetime(created_ts, 'unixepoch') as created_ts,sender_id,receiver_id,status,message FROM inbox;" >> memos_mysql.sql
+sqlite3 "$litedb" ".mode insert memo_relation" "select memo_id,related_memo_id,type from memo_relation;" >> memos_mysql.sql
 
+sqlite3 "$litedb" ".mode insert reaction" "select id,datetime(created_ts, 'unixepoch') as created_ts,creator_id,content_id,reaction_type from reaction;" >> memos_mysql.sql
+
+sqlite3 "$litedb" ".mode insert inbox" "SELECT id,datetime(created_ts, 'unixepoch') as created_ts,sender_id,receiver_id,status,message FROM inbox;" >> memos_mysql.sql
 
 if [[ "$REPLY" == 2 ]]
 then
@@ -59,9 +64,17 @@ fi
 
 if [[ "$REPLY" == 3 ]]
 then
+  read -p "请输入MySQL数据库名（不填默认为memos_prod）: " mydb
+  if [[ -z "$mydb" ]]; then
+    mydb="memos_prod"
+  fi
+
+
   read -p "请输入MySQL用户名（不填默认为root）: " myname
   if [[ -z "$myname" ]]; then
     myname=root
   fi
-  mysql -u "$myname" -p memos_prod < memos_mysql.sql
+  echo "MySQL: $mydb"
+  echo "User: $myname"
+  mysql -u "$myname" -p "$mydb" < memos_mysql.sql
 fi
