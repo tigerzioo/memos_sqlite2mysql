00e2c7bbd7184d24f4b008775b724ece9f939830
tigerzioo <148376507+tigerzioo@users.noreply.github.com>
2024-04-09 18:49:12 -0500

    Beta，仍在测试中

--- mysql2sqlite/memos_mysql2sqlite.sh
+++ mysql2sqlite/memos_mysql2sqlite.sh
@@ -1,27 +1,116 @@
 #!/bin/bash
 
+echo " "
+echo " "
+echo "+++++++++++++++++ Memos MySQL 2 SQLite ++++++++++++++++++++++"
+echo "提示："
+echo "MySQL必需安装在本机，数据库名可自己输入，默认为memos_prod，SQLite数据库文件必需为memos_prod.db。"
+echo "此Bash脚本文件要和SQLite数据库文件放在同一目录下。"
+echo " "
+columns=12
+PS3="请选择SQLite导入方式："
+select action in "退出" "仅生成SQLite数据库的导入脚本，稍后自己手动导入" "直接导入到SQLite数据库"
+do
+  #  echo "选择的导入方式：$action"
+  #  echo "选择的序号：$REPLY"
+  echo "+++++++++++++++++++"
+  echo " "
+
+
+  if [[ "$REPLY" == 1 ]]
+  then
+    echo "退出！"
+    echo "+++++++++++++++++++"
+    echo " "
+    exit
+  fi
+  if [[ "$REPLY" != 2 && "$REPLY" != 3 ]]
+  then
+    echo "无效，请重新选择！"
+  else
+    break
+  fi
+done
+
+read -p "请输入MySQL数据库名（回车默认为memos_prod）: " mydb
+if [[ -z "$mydb" ]]; then
+  mydb="memos_prod"
+fi
+
+read -p "请输入MySQL用户名（回车默认为root）: " myname
+if [[ -z "$myname" ]]; then
+  myname="root"
+fi
+echo -n "Password："
+read -s mypass
+if [[ -z "$mypass" ]]; then
+  mypass=""
+fi
+echo ""
+echo ""
+echo "MySQL: $mydb"
+echo "User: $myname"
+# echo "Pass: $mypass"
+
+
 # create insert scripts
 # USER
-mysql -u root --disable-column-names -B -e "select 'INSERT INTO user (id,created_ts,updated_ts,row_status,username,role,email,nickname,password_hash,avatar_url,description) VALUES' union all SELECT concat('(',id,',',UNIX_TIMESTAMP(created_ts),',',UNIX_TIMESTAMP(updated_ts),',''',row_status,''',''',username,''',''',role,''',''',email,''',''',nickname,''',''',password_hash,''',''',avatar_url,''',''',description,'''),') as iq FROM memos_prod.user" > memos_2sqlite.sql
+mysql -u "$myname" -p"$mypass" --disable-column-names -B -e "select 'INSERT INTO user (id,created_ts,updated_ts,row_status,username,role,email,nickname,password_hash,avatar_url,description) VALUES' union all SELECT concat('(',id,',',UNIX_TIMESTAMP(created_ts),',',UNIX_TIMESTAMP(updated_ts),',''',row_status,''',''',username,''',''',role,''',''',email,''',''',nickname,''',''',password_hash,''',''',avatar_url,''',''',description,'''),') as iq FROM $mydb.user" > memos_2sqlite.sql
 
-# remove the comma at the end of the file
-sed -i '$ s/.$//' memos_2sqlite.sql
+# Replace the last comma with semi colon at the end of the file
+sed -i '$ s/.$/;/' memos_2sqlite.sql
 
 # TAG
-mysqldump -u root --no-create-info --skip-triggers --compact --extended-insert=FALSE memos_prod tag >> memos_2sqlite.sql
+mysqldump -u "$myname" -p"$mypass" --no-create-info --skip-triggers --compact --extended-insert=FALSE "$mydb" tag >> memos_2sqlite.sql
 
 # MEMO
-mysql -u root --disable-column-names -B -e "select 'INSERT INTO memo (id,uid,creator_id,created_ts,updated_ts,row_status,content,visibility) VALUES' union all SELECT concat('(',id,',''',uid,''',',creator_id,',',UNIX_TIMESTAMP(created_ts),',',UNIX_TIMESTAMP(updated_ts),',''',row_status,''',''',content,''',''',visibility,'''),') as iq FROM memos_prod.memo" >> memos_2sqlite.sql
+mysql -u "$myname" -p"$mypass" --disable-column-names -B -e "select 'INSERT INTO memo (id,uid,creator_id,created_ts,updated_ts,row_status,content,visibility) VALUES' union all SELECT concat('(',id,',''',uid,''',',creator_id,',',UNIX_TIMESTAMP(created_ts),',',UNIX_TIMESTAMP(updated_ts),',''',row_status,''',''',content,''',''',visibility,'''),') as iq FROM $mydb.memo" >> memos_2sqlite.sql
 
-sed -i '$ s/.$//' memos_2sqlite.sql
+sed -i '$ s/.$/;/' memos_2sqlite.sql
 
 # RESOURCE
-mysql -u root --disable-column-names -B -e "select 'INSERT INTO resource (id, resource_name, creator_id, created_ts, updated_ts, filename, blob, external_link, type, size, internal_path, memo_id) VALUES' union all SELECT concat('(',id,',''',resource_name,''',',creator_id,',',UNIX_TIMESTAMP(created_ts),',',UNIX_TIMESTAMP(updated_ts),',''',filename,''',',concat('x''',hex(\`blob\`)),''',''',external_link,''',''',type,''',',size,',''',internal_path,''',',memo_id,'),') as iq FROM memos_prod.resource" >> memos_2sqlite.sql
+if [ $(mysql -u "$myname" -p"$mypass" $mydb -sse "select count(*) from resource;") -gt 0 ];
+then
+  mysql -u "$myname" -p"$mypass"  --disable-column-names -B -e "select 'INSERT INTO resource (id,uid,creator_id,created_ts,updated_ts,filename,blob,external_link,type,size,internal_path,memo_id) VALUES' union all SELECT concat('(',id,',''',uid,''',',creator_id,',',UNIX_TIMESTAMP(created_ts),',',UNIX_TIMESTAMP(updated_ts),',''',filename,''',',concat('x''',hex(\`blob\`)),''',''',external_link,''',''',type,''',',size,',''',internal_path,''',',memo_id,'),') as iq FROM $mydb.resource" >> memos_2sqlite.sql
 
-sed -i '$ s/.$//' memos_2sqlite.sql
+  sed -i '$ s/.$/;/' memos_2sqlite.sql
+fi
+
+# memo_organizer
+mysqldump -u "$myname" -p"$mypass" --no-create-info --skip-triggers --compact --extended-insert=FALSE "$mydb" memo_organizer >> memos_2sqlite.sql
+
+# memo_relation
+mysqldump -u "$myname" -p"$mypass" --no-create-info --skip-triggers --compact --extended-insert=FALSE "$mydb" memo_relation >> memos_2sqlite.sql
+
+# REACTION
+if [ $(mysql -u "$myname" -p"$mypass" $mydb -sse "select count(*) from reaction;") -gt 0 ];
+then
+  mysql -u "$myname" -p"$mypass" --disable-column-names -B -e "select 'INSERT INTO reaction (id,created_ts,creator_id,content_id,reaction_type) VALUES' union all SELECT concat('(',id,',',UNIX_TIMESTAMP(created_ts),',',creator_id,',',content_id,',''',reaction_type,'''),') as iq FROM $mydb.reaction" >> memos_2sqlite.sql
+
+  sed -i '$ s/.$/;/' memos_2sqlite.sql
+fi
+
+# INBOX
+if [ $(mysql -u "$myname" -p"$mypass" $mydb -sse "select count(*) from inbox;") -gt 0 ];
+then
+  mysql -u "$myname" -p"$mypass" --disable-column-names -B -e "select 'INSERT INTO inbox (id,created_ts,sender_id,receiver_id,status,message) VALUES' union all SELECT concat('(',id,',',UNIX_TIMESTAMP(created_ts),',',sender_id,',',receiver_id,',''',status,''',''',message,'''),') as iq FROM $mydb.inbox" >> memos_2sqlite.sql
+
+  sed -i '$ s/.$/;/' memos_2sqlite.sql
+fi
 
 
+if [[ "$REPLY" == 2 ]]
+then
+  echo " "
+  echo "========脚本已生成，保存在当前目录，文件名为memos_2sqlite.sql"
+  echo "+++++++++++++++++++"
+  echo " "
+fi
 
+if [[ "$REPLY" == 3 ]]
+then
+  # Load data to SQLite database
+  sqlite3 memos_prod.db < memos_2sqlite.sql
 
-# Load data to SQLite database
-sqlite3 memos_prod.db < memos_2sqlite.sql
+  sqlite3 memos_prod.db "PRAGMA wal_checkpoint(PASSIVE);" >/dev/null 2>&1 
+fi
