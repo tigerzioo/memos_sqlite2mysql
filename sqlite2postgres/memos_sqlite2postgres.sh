b76077e5529c40be575616386260762e5d58ef19
tigerzioo <148376507+tigerzioo@users.noreply.github.com>
2024-07-30 14:20:13 -0500

    Fixed: PK error when inserting a new memo after migration

--- sqlite2postgres/memos_sqlite2postgres.sh
+++ sqlite2postgres/memos_sqlite2postgres.sh
@@ -116,4 +116,11 @@ then
   echo "PostgreSQL用户名 (user): $myname"
   # psql -d memos < memos_2post.sql 
   psql -d "$mydb" -U "$myname" -W < memos_2post.sql
+
+  echo " "
+  echo "========数据转换完成（Data migration completed!）"
+  echo "========重置table自动id起始值（Set table serial column start sequence）"
+  echo " "
+  psql -d "$mydb" -U "$myname" -W -c "SELECT setval('user_id_seq', COALESCE((SELECT MAX(id)+1 FROM user), 1), false);SELECT setval('memo_id_seq', COALESCE((SELECT MAX(id)+1 FROM memo), 1), false);SELECT setval('resource_id_seq', COALESCE((SELECT MAX(id)+1 FROM resource), 1), false);SELECT setval('reaction_id_seq', COALESCE((SELECT MAX(id)+1 FROM reaction), 1), false);SELECT setval('inbox_id_seq', COALESCE((SELECT MAX(id)+1 FROM inbox), 1), false);"
+
 fi
